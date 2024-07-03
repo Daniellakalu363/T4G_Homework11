7c0d186ae9e5a5ea183ea2e8669f0b9345f464f1
Daniellakalu363 <daniellakalu4@gmail.com>
2024-07-03 12:54:08 -0700

    Add all files

---
+++ src/index.js
@@ -0,0 +1,24 @@
+let heading =document.getElementById("heading");
+heading.style.backgroundColor="blue";
+heading.innerHTML="Learning how to manipulate the DOM using JS 💪🏽";
+
+let second =document.getElementById("second");
+second.style.backgroundColor="beige";
+second.style.color="black";
+second.innerHTML="i know now what the DOM is 🙌🏽";
+
+let third =document.getElementById("third");
+third.style.backgroundColor="yellow";
+third.innerHTML="I know how to access/select elements in the DOM 👊🏾";
+
+let fouth =document.getElementById("fouth");
+fouth.style.backgroundColor="green";
+fouth.innerHTML="I know how to use the style methods to change the style of an element in the DOM ✅";
+
+let firth =document.getElementById("firth");
+firth.style.backgroundColor="purple";
+firth.innerHTML="I know how the styling properties differ in JS from CSS 🤙🏽";
+
+let sixth =document.getElementById("sixth");
+sixth.style.backgroundColor="black"
+sixth.innerHTML="Yes I did it!! I manipulated the DOM and styled my elements 😍😍😍😍";
